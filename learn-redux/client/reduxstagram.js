5d3fdf1b169302079900419ebe882fa7c593c68e
brian <brian@ultra.me>
2017-12-06 14:03:17 -0800

    first - simple react redux tutorial

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+//webpack will generate css js file
+import css from './styles/style.styl';
+//import comps
+import Main from './components/Main';
+
+render(
+	<Main/>,
+	document.getElementById('root')
+);
