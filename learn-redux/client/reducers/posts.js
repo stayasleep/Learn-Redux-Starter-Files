e68d692c816acdff4023fe7214eb95b08d0f8775
brian <brian@ultra.me>
2017-12-07 09:55:35 -0800

    finished the first reducer for posts - work

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,9 +1,19 @@
-// import {} from '../actions/types';
+import {INCREMENT_LIKES} from '../actions/types';
 
 //es6 default params but can also declare above here as a var
 function posts(state=[], action){
 	console.log('pos',state,action);
-	return state;
+	switch(action.type){
+		case INCREMENT_LIKES:
+			const i = action.index;
+			return [
+				...state.slice(0,i), //Obj before our target
+				{...state[i], likes: state[i].likes +1}, //the targeted obj in the array
+				...state.slice(i+1) //Objs after the target
+			]
+		default:
+			return state;
+	}
 }
 
 export default posts;
